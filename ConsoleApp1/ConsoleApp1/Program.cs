9cf17563faad818ae984454ecb5a573143d8a1a5
Juank1221 <jcchicle@hotmail.com>
2026-02-10 19:07:17 -0600

    primer codigo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola Programación 5 UTC");
+int edad = 43;
+Console.WriteLine("Juank");
+Console.WriteLine("Barrantes");
+Console.WriteLine(edad);
+
