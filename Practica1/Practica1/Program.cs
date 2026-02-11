688b388415b4b4ece221878071635ddb6c9cc710
Juank1221 <jcchicle@hotmail.com>
2026-02-10 19:51:49 -0600

    cambio10

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Juan Carlos");
+Console.WriteLine("Fecha Y Hora");
+Console.WriteLine(DateTime.Now);
+Console.WriteLine("Prueba para examen");
